37311da1244b8a7493e40f54ddbd8f9c5e022a6b
rakeshmadu <rakeshmadu24@gmail.com>
2019-07-15 14:52:47 +0530

    ABCD

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -120,4 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 django_heroku.settings(locals())
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
